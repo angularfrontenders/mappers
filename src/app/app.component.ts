c0481f9bc70dda12234a0d7d56b6c80fd8667b21
Cesar Marin <cesarmj1974@gmail.com>
2019-12-25 21:05:09 +0100

    mappers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EntityMapperService } from './mapper/entityMapper.service';
+import { IReadEntity } from './mapper/iReadEntity';
+import { IEntity } from './mapper/iEntity';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mappers';
+export class AppComponent implements OnInit {
+
+    title = 'mappers';
+
+    public constructor(private _entityMapperService: EntityMapperService) {
+
+    }
+
+    ngOnInit(): void {
+        // Map entity
+        const readEntity: IReadEntity = {
+            id: '123',
+            name: 'example mapper',
+            birthDate: '2000/01/01',
+            type: 'person',
+            version: 2
+        };
+        const entity: IEntity = this._entityMapperService.transform(readEntity);
+
+        // Map array of entities
+        const arrayEntities: Array<IReadEntity> = [readEntity];
+        const entities: Array<IEntity> = this._entityMapperService.transform(arrayEntities);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntityMapperService } from './mapper/entityMapper.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [EntityMapperService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
